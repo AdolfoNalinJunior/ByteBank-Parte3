77e0da889139826344ad93ca6c13f9c9c865f18e
AdolfoNalinJunior <adolfonalin22@outlook.com>
2022-04-30 20:29:45 -0300

    commit 30/04/2022

---
+++ ByteBank/GerencioadorBonificacao.cs
@@ -0,0 +1,28 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class GerencioadorBonificacao
+    {
+        private double _totalBonificacao;
+        public void Registrar(Funcionario funcionario)
+        {
+            _totalBonificacao += funcionario.GetBonificacao();
+        }
+
+        public void Registrar(Diretor diretor)
+        {
+            _totalBonificacao += diretor.GetObterBonificacao();
+        }
+
+        public double TotalBonificacao()
+        {
+            return _totalBonificacao;
+        }
+    }
+}
---
+++ ByteBank/Program.cs
@@ -0,0 +1,40 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GerencioadorBonificacao gerenciador = new GerencioadorBonificacao();  
+
+            Funcionario carlos = new Funcionario();
+            carlos.Nome = "Carlos";
+            carlos.Cpf = "546.879.157-20";
+            carlos.Salario = 2000;
+
+            Console.WriteLine(carlos.Nome);
+            Console.WriteLine(carlos.GetBonificacao());
+
+            gerenciador.Registrar(carlos);
+
+            Diretor roberta = new Diretor();
+            roberta.Nome = "Roberta";
+            roberta.Cpf = "454.658.148-3";
+            roberta.Salario = 5000;
+
+            Console.WriteLine(roberta.Nome);
+            Console.WriteLine(roberta.GetObterBonificacao());
+
+            gerenciador.Registrar(roberta);
+
+            Console.WriteLine("Total da Bonificação: " + gerenciador.TotalBonificacao());
+
+        }
+    }
+}
